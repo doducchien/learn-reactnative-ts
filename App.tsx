3a2b789227c3a43f195fb0dfa32d350ef09193b1
cinterface <chiendd226@gmail.com>
2024-01-05 00:53:01 +0700

    width, height

--- App.tsx
+++ App.tsx
@@ -1,17 +1,17 @@
 import React, { Children } from 'react';
-import {ScrollView, Text, View, Image, TextInput, StyleSheet, FlatList, SectionList} from 'react-native';
+import { ScrollView, Text, View, Image, TextInput, StyleSheet, FlatList, SectionList } from 'react-native';
 import Cat from './src/component/Cat';
 import PizzaTranslator from './src/component/PizzaTranslator';
 
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     paddingTop: 22,
     backgroundColor: 'white'
   },
-  sectionHeader:{
-    paddingVertical: 2, 
+  sectionHeader: {
+    paddingVertical: 2,
     paddingHorizontal: 10,
     fontSize: 14,
     fontWeight: 'bold',
@@ -25,16 +25,16 @@ const styles = StyleSheet.create({
 })
 
 const data = [
-  {key: 'Devin'},
-  {key: 'Dan'},
-  {key: 'Dominic'},
-  {key: 'Jackson'},
-  {key: 'James'},
-  {key: 'Joel'},
-  {key: 'John'},
-  {key: 'Jillian'},
-  {key: 'Jimmy'},
-  {key: 'Julie'},
+  { key: 'Devin' },
+  { key: 'Dan' },
+  { key: 'Dominic' },
+  { key: 'Jackson' },
+  { key: 'James' },
+  { key: 'Joel' },
+  { key: 'John' },
+  { key: 'Jillian' },
+  { key: 'Jimmy' },
+  { key: 'Julie' },
 ]
 
 const sections = [
@@ -42,37 +42,60 @@ const sections = [
     title: 'D', data: ['Devin', 'Dan', 'Dominic']
   },
   {
-    title: 'J', data: ['Jackson','James','Jillian','Jimmy','Joel','John','Julie'],
+    title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie'],
   }
 ]
 
-const App = ()=>{
+const App = () => {
 
 
-  return(
+  return (
     // <View>
     //   <Cat name='Mom'></Cat>
     //   <Cat name='Meo'></Cat>
 
     //   <PizzaTranslator/>
     // </View>
-    <View style={styles.container}>
-      {/* <FlatList
-        data={data}
-        renderItem={child => <Text style={styles.item}>{child.item.key}</Text>}
-      /> */}
+    // <View style={styles.container}>
+    //   <FlatList
+    //     data={data}
+    //     renderItem={child => <Text style={styles.item}>{child.item.key}</Text>}
+    //   />
 
-      <SectionList
+    //   <SectionList
       
-        sections={sections}
-        renderItem={child => <Text style={styles.item}>{child.item}</Text>}
-        renderSectionHeader={child => <Text style={styles.sectionHeader}>{child.section.title}</Text> }
-        keyExtractor={item => {
-          console.log(`basicListEntry-${item}`);
-          return `basicListEntry-${item}`
-        }}
-      />
-    </View>
+    //     sections={sections}
+    //     renderItem={child => <Text style={styles.item}>{child.item}</Text>}
+    //     renderSectionHeader={child => <Text style={styles.sectionHeader}>{child.section.title}</Text> }
+    //     keyExtractor={item => {
+    //       console.log(`basicListEntry-${item}`);
+    //       return `basicListEntry-${item}`
+    //     }}
+    //   />
+
+    //   <View style={{ height: 50, width: 50, backgroundColor: 'powderblue' }} />  
+    //   <View style={{ height: 100, width: 100, backgroundColor: 'skyblue' }} />  
+
+    //   <View style={{ height: 150, width: 150, backgroundColor: 'steelblue' }} />  
+
+    // </View>
+    // <View style={{flex: 1}}>
+    //   <View style={{flex:1, backgroundColor: 'powderblue'}}/>  
+    //   <View style={{flex:2, backgroundColor: 'skyblue'}}/>  
+    //   <View style={{flex:3, backgroundColor: 'steelblue'}}/>  
+
+
+
+    // </View>
+
+    <View style={{height: '100%'}}>
+    <View style={{height: '15%', backgroundColor: 'powderblue'}}/>  
+    <View style={{height: '35%', width:'66%', flex:2, backgroundColor: 'skyblue'}}/>  
+    <View style={{height: '50%', width: '33%', flex:3, backgroundColor: 'steelblue'}}/>  
+
+
+
+  </View>
   )
 }
 export default App;
